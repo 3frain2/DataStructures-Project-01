1ce70a46547c443169c34920bcfc8aebb81067da
Efrain Vega <51217130+3frain2@users.noreply.github.com>
2019-11-15 01:41:20 -0600

    Add files via upload

--- Proyecto-Estructuras/src/Modelo/Personaje.java
+++ Proyecto-Estructuras/src/Modelo/Personaje.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package Modelo;
-
+import java.awt.Graphics2D;
 /**
  *
  * @author efrai
  */
-public class Personaje {
+
+public interface Personaje {
+  
+  double getX();
+  double getY();
+  int getR();
+  boolean isDead();
+  void hit();
+  void update();
+  abstract void draw(Graphics2D g);
   
 }
--- Proyecto-Estructuras/src/Modelo/PersonajeMarine.java
+++ Proyecto-Estructuras/src/Modelo/PersonajeMarine.java
@@ -5,10 +5,87 @@
  */
 package Modelo;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author efrai
  */
-public class PersonajeMarine {
+public class PersonajeMarine implements Personaje {
+  private double x;
+  private double y;
+  private int r;
+
+  private double dx;
+  private double dy;
+  private double rad;
+  
+  
+  private int hp;
+  private int ataque;
+  private double speed;
+  
+  private BufferedImage enemigoImagen;
+
+  private boolean ready;
+  private boolean dead;
+    
+  public PersonajeMarine() throws IOException {
+    
+    r = 40;
+    hp = 100;
+    ataque = 80;
+    speed = 2;
+    enemigoImagen = ImageIO.read(new File("src/Imagenes/Enemigo/Salyut.png"));
+    
+    double angle = Math.random() * 140 + 20;
+    rad = Math.toRadians(angle);
+    
+    dx = Math.cos(rad) * speed;
+    dy = Math.sin(rad) * speed;
+  }
+
+  @Override
+  public double getX() {
+    return x;
+  }
+
+  @Override
+  public double getY() {
+    return y;
+  }
+
+  @Override
+  public int getR() {
+    return r;
+  }
+
+  @Override
+  public boolean isDead() {
+    return dead;
+  }
+  
+  @Override
+  public void hit() {
+    hp--;
+    if(hp <= 0) {
+      dead = true;
+    }
+  }
+
+  @Override
+  public void update() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void draw(Graphics2D g) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
   
 }
